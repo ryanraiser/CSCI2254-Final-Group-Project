e66727a777aa6fd15fcc14ef6b52819e786f0189
Ryan Raiser <156840783+ryanraiser@users.noreply.github.com>
2025-11-20 04:28:45 +0000

    pre-recipe scaling implementation

--- home.js
+++ home.js
@@ -1,15 +1,30 @@
-fetch("recipes.json")
-  .then((r) => r.json())
-  .then((data) => {
-    const recipes = data.recipes;
-    renderRecipes(recipes);
-  });
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("recipes.json")
+    .then((r) => r.json())
+    .then((data) => {
+      const recipes = data.recipes;
+      renderRecipes(recipes);
+    });
+});
 
 function createRecipe(rec) {
   return `
     <div class="card">
         <h2>${rec.title}</h2>
-        <p>${rec.description}</p>
+        <p class="desc">${rec.description}</p>
+
+        <input type="number" class="servings-input" min="1" placeholder="Intended Serving Size">
+
+        <h3>Ingredients</h3>
+        <ul class="ing-list">
+          ${rec.ingredients
+            .map((item) => `<li>${item.qty} ${item.item}</li>`)
+            .join("")}
+        </ul>
+
+        <h3>Instructions</h3>
+        <p class="inst">${rec.instructions}</p>
+      </div>
     </div>
   `;
 }
